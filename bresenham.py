9826cf80fb3554723ef62090b01e7c33b4c5e07a
Lakshamana <guitrompa1@gmail.com>
2019-03-20 18:27:40 -0300

    Change point dict to class

--- bresenham.py
+++ bresenham.py
@@ -1,38 +1,38 @@
 # coding: utf-8
 # Algoritmo de Bresenham
-from numpy import poly1d
-
-order = int(input('Digite a ordem da malha de desenho: '))
-
-malha = [[0 for i in range(order)] for i in range(order)]
 
 """
 ponto = {'x': xvalue, 'y': yvalue}
 """
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 def prtab(tab):
     for line in tab: 
         print(line)
 
 
-def calcAngular(p1: dict, p2: dict):
-    return (p2['x'] - p1['x']) / (p2['y'] - p1['y'])
+def calcAngular(p1: Point, p2: Point):
+    return (p2.x - p1.x) / (p2.y - p1.y)
 
 
-def reflexao(p1: dict, p2: dict):
+def reflexao(p1: Point, p2: Point):
     m = calcAngular(p1, p2)
     trocaxy = trocax = trocay = False
     if m > 1 or m < -1:
-        p1['x'], p1['y'] = p1['y'], p1['x']
-        p2['x'], p2['y'] = p2['y'], p2['x']
+        p1.x, p1.y = p1.y, p1.x
+        p2.x, p2.y = p2.y, p2.x
         trocaxy = True
-    if p1['x'] > p2['x']:
-        p1['x'] = -p1['x']
-        p2['x'] = -p2['x']
+    if p1.x > p2.x:
+        p1.x = -p1.x
+        p2.x = -p2.x
         trocax = True
-    if p1['y'] > p2['y']:
-        p1['y'] = -p1['y']
-        p2['y'] = -p2['y']
+    if p1.y > p2.y:
+        p1.y = -p1.y
+        p2.y = -p2.y
         trocay = True
     return (p1, p2, trocaxy, trocax, trocay)
 
@@ -41,36 +41,45 @@ def reflexaoInv(points: list, trocaxy, trocax, trocay):
     new_points = []
     for p in points:
         if trocaxy:
-            p['x'], p['y'] = p['y'], p['x']
+            p.x, p.y = p.y, p.x
         if trocax:
-            p['x'] = -p['x']
+            p.x = -p.x
         if trocay:
-            p['y'] = -p['y']
+            p.y = -p.y
         new_points.append(p)
     return new_points
 
 
-def plot(p: dict):
-    global malha
-    malha[p['x']][p['y']] = 1
+def plot(tab, p: Point):
+    tab[p.x][p.y] = 1
 
-def bresenham(p1: dict, p2: dict):
+def bresenham(malha, p1: Point, p2: Point):
     (p1, p2, trocaxy, trocax, trocay) = reflexao(p1, p2)
-    x = p1['x']; y = p1['y']
-    init_point = {'x': x, 'y': y}
+    x = p1.x
+    y = p1.y
+    init_point = Point(x, y)
     m = calcAngular(p1, p2)
     e = m - 0.5
-    plot(init_point)
+    plot(malha, init_point)
     p = []
-    while x < p2['x']:
+    while x < p2.x:
         if e >= 0:
             y += 1
             e -= 1
         x += 1
         e += m
-        new_point = {'x', x, 'y', y}
-        plot(new_point)
+        new_point = Point(x, y)
         p.append(new_point)
     p = reflexaoInv(p, trocaxy, trocax, trocay)
+    for point in p: 
+        plot(malha, point)
     return p
 
+order = int(input('Digite a ordem da malha de desenho: '))
+malha = [[0 for i in range(order)] for i in range(order)]
+prtab(malha)
+p1 = Point(0, 0)
+p2 = Point(2, 3)
+bresenham(malha, p1, p2)
+print()
+prtab(malha)
